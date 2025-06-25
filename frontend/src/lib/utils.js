551d09995020079b244f518f6b2719a978f453f4
SolCraft Nexus <solcraft@nexus.com>
2025-06-25 07:30:07 -0400

    Fix utils.js to remove tailwind-merge dependency

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,4 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwindcss-merge"
-
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return inputs.filter(Boolean).join(' ')
 }
 
